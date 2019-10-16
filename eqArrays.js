2459c9eb91ca1a41b502bbc13c4a7a47836e7547
hanuz06 <lee1975uz@gmail.com>
2019-10-16 00:47:27 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const eqArrays = (arr1, arr2) => {
+  let actual = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      arr1[i] === arr2[i] ? actual : actual = false;
+      //console.log(actual);
+    }
+  } else {
+    actual = false;
+  }
+
+  return actual;
+};
+
+
+
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`😜  Assertion Passed: ${actual} === ${expected}`) : console.log(`🤨  Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, '2', 3], [1, 2, 3]), true);
