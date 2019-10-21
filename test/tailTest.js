1a6d5f0cc6058e697c80bca33f6b74c766e6a566
hanuz06 <lee1975uz@gmail.com>
2019-10-21 20:24:01 +0000

    refactored tailTest.js with #jamaal729 #granttaylor448

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,31 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+//const assertEqual = require('../assertEqual');
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
 
-const result = tail(words); // no need to capture the return value since we are not checking it
+// const result = tail(words); // no need to capture the return value since we are not checking it
 
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+describe("#tail", () => {
+  it("returns 'Lighthouse' in an array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words)[0], "Lighthouse");
+  });
+
+  it("returns 'Labs' in an array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words)[1], "Labs");
+  });
+
+  it("returns 2 as array length", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.deepEqual(tail(words).length, 2);
+  });
+  
+});
