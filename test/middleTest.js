7bc61b3a6398c970dbc0acf868a8a784b8bf63ed
hanuz06 <lee1975uz@gmail.com>
2019-10-21 20:47:35 +0000

    refactored middeTest to use mocha and chai with #jamaal729 and #granttaylor448

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,24 +1,17 @@
-const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle.js')
+const assert = require('chai').assert;
 
-// const eqArrays = (arr1, arr2) => {
-//   let actual = true;
-//   if (arr1.length === arr2.length) {
-//     for (let i = 0; i < arr1.length; i++) {
-//       arr1[i] === arr2[i] ? actual : actual = false;
-//     }
-//   } else {
-//     actual = false;
-//   }
-//   return actual;
-// };
-
-// const assertArraysEqual = function(arr1, arr2) {
-//   eqArrays(arr1, arr2) ? console.log(`😜  Assertion Passed: ${arr1} === ${arr2}`) : console.log(`🤨  Assertion Failed: ${arr1} !== ${arr2}`);
-// };
-
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,5]), [2,3]);
-assertArraysEqual(middle([1,2,3,4]), [2]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+describe("#middle", () => {
+  it("returns 2 given an array [1,2,3]", () => {
+    const array = [1,2,3];
+    assert.deepEqual(middle(array), [2]);
+  });
+  it("returns [] given an array [1,2]", () => {
+    const array = [1,2];
+    assert.deepEqual(middle(array), []);
+  });
+  it("returns [] given an array [1,2,3,4,5,6]", () => {
+    const array = [1,2,3,4,5,6];
+    assert.deepEqual(middle(array), [3,4]);
+  });
+});
