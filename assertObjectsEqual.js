7b9fcdd85c8be47f176d636cdf9ac2b79deea017
hanuz06 <lee1975uz@gmail.com>
2019-10-22 00:04:25 +0000

    added README.md

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = (arr1, arr2) => {
+sdconst eqArrays = (arr1, arr2) => {
   let result = true;
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
--- head.js
+++ head.js
@@ -1,7 +1,3 @@
-//const assertEqual = function(actual, expected) {
-//   actual === expected ? console.log(`😜  Assertion Passed: ${actual} === ${expected}`) : console.log(`🤨  Assertion Failed: ${actual} !== ${expected}`);
-// };
-
 const head = arr => arr[0];
 
 module.exports =  head;
